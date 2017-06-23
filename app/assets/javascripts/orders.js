44311facab56d4ca39858323ea7ae9f81047c3dd
John Doe <johntravers87@gmail.com>
2017-06-22 21:05:02 -0400

    first html and css styling to drink menu page

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -1,2 +1,28 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$( document ).ready(function() {
+    console.log( "reeeady!" );
+
+  $('#code').click(function () {
+    var val = 30 + '%';
+    $('.progress-bar').width(val).text(val);
+    $('.message').append('<h3>Your order was Received!</h3>')
+  });
+
+  $('#code1').click(function () {
+    var val = 30 + '%';
+    $('.progress-bar').width(val).text(val);
+    $('.message').append('<h3>Your order was Received!</h3>')
+  });
+  
+  $('#code2').click(function () {
+    var val1 = 66 + '%';
+    $('.progress-bar').width(val1).text(val1)
+  });
+
+  $('#code3').click(function () {
+    var val2 = 100 + '%';
+    $('.progress-bar').width(val2).text(val2)
+  });
+});
