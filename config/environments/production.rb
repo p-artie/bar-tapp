b1d1342b31212c929444d8b30923832e7fa6cb2a
Artie Polanco <artiepolanco@gmail.com>
2017-06-26 09:28:15 -0400

    added 2 lines to production to enable background images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,8 @@ Rails.application.configure do
   # `config/secrets.yml.key`.
   config.read_encrypted_secrets = true
 
+  config.serve_static_assets = true
+
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
@@ -28,7 +30,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
